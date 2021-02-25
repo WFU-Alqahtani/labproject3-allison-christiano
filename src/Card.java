59a868f5b4cab9e31caec892f9b94059c1fa8166
allisonchristiano <chrial20@wfu.edu>
2021-02-25 11:34:37 -0500

    minimal changes from starter code implemented

--- src/Card.java
+++ src/Card.java
@@ -1,7 +1,14 @@
+import java.util.Arrays;
+
 public class Card {
     // ranks are ordered Ace-low and Queen-high
     private final String ranks[] ={"Ace","2","3","4","5","6","7","8","9","10","Jack","King","Queen"};
 
 // suits are ordered alphabetically
     private final String suits[] = {"Clubs","Diamonds","Hearts","Spades"};
+
+    @Override
+    public String toString() {
+        return String.format("%s of %s", ranks[0], suits[0]);//NEED TO FIX SO DOESNT RETURN ranks[0] and suits[0] ALL THE TIME
+    }
 }
--- src/Deck.java
+++ src/Deck.java
@@ -5,4 +5,19 @@ public class Deck {
     private ArrayList<Card> cards=new ArrayList < Card >();
     private int arraySize;
     private int cardsLeft;
+    public Deck()
+    {
+        this.arraySize = -1;
+        this.cardsLeft = -1;
+    }
+    public Deck(ArrayList<Card> cards, int arraySize, int cardsLeft)
+    {
+        for (int i = 0; i < cards.size(); i++)
+        {
+            Card card = new Card();
+            this.cards.add(card);
+        }
+        this.arraySize = 52;
+        this.cardsLeft = 52;
+    }
 }
